0e8273647ef40d2a93608f57a19f50fdc5901d0c
pbousquets <pbousquetsm@gmail.com>
2019-10-10 14:42:57 +0200

    Allow bed format as rois list input

--- scripts/load_functions.sh
+++ scripts/load_functions.sh
@@ -168,61 +168,62 @@ extract_minibam(){
 	echo ${time}: Generating ${2} minibams... | tee -a pipeline.log
 
 	case=$1
-	sample=$2
-	type=$3
-	blat_coords=$4 #Dir of blat coords of each exon
-	miniFasta_dir=$5
-	rois_list=$6 #List of regions of interest
-	threads=$7
-	#list of regions
-	lines=$(cat $rois_list | wc -l )
+		sample=$2
+		type=$3
+		blat_coords=$4 #Dir of blat coords of each exon
+		miniFasta_dir=$5
+		rois_list=$6 #List of regions of interest
+		threads=$7
+		#list of regions
+		lines=$(cat $rois_list | wc -l )
 
-	cat $rois_list | while read -r file #Create a tmp minibam for each region
-	do
-	    if [ -f ${blat_coords}/${file} ]
-	    then
-	        lines=$((lines - 1))
-	        #Header
-	        SM=${sample}
-	        PL=illumina
-	        LB=WGS #Or WES
-	        PU=${sample}
-	        RG="@RG\\tID:${sample}\\tSM:${SM}\\tPL:${PL}\\tLB:${LB}\\tPU:${PU}"
-	        allCoords=$(cat ${blat_coords}/${file}) #Save its coords
-	        samtools view -u -G 0x400 -G 0x4 ${sample} $allCoords | samtools fastq -N - 2>/dev/null|  bwa mem -t ${threads} -R ${RG} ${miniFasta_dir}/${file}.fa - 2>/dev/null | samtools view -uS - > tmp_files/${file}.bam
-	        printf "\r Initial time: ${time}. Files left: $lines        "
-	    else
-	        echo "~" ${blat_coords}/${file} does not exist >> pipeline.log
-	    fi
-	done
-	time=$(date +%x%t%X)
-	echo ${time}: Merging ${case} minibams. This may take a while | tee -a pipeline.log
+		cat $rois_list | tr ':-' '\t' | while read -r chr st end #Create a tmp minibam for each region
+		do
+			coord=$(echo ${chr}:${st}-${end}) #By reading this way the rois list file, we can use both chr:st-end and bed format
+			if [ -f ${blat_coords}/${file} ]
+			then
+				lines=$((lines - 1))
+				#Header
+				SM=${sample}
+				PL=illumina
+				LB=WGS #Or WES
+				PU=${sample}
+				RG="@RG\\tID:${sample}\\tSM:${SM}\\tPL:${PL}\\tLB:${LB}\\tPU:${PU}"
+				allCoords=$(cat ${blat_coords}/${file}) #Save its coords
+				samtools view -u -G 0x400 -G 0x4 ${sample} $allCoords | samtools fastq -N - 2>/dev/null|  bwa mem -t ${threads} -R ${RG} ${miniFasta_dir}/${file}.fa - 2>/dev/null | samtools view -uS - > tmp_files/${file}.bam
+				printf "\r Initial time: ${time}. Files left: $lines        "
+			else
+				echo "~" ${blat_coords}/${file} does not exist >> pipeline.log
+			fi
+		done
+		time=$(date +%x%t%X)
+		echo ${time}: Merging ${case} minibams. This may take a while | tee -a pipeline.log
 
-	iter=0
-	cd tmp_files
-	while [ $(ls | grep -v tmp | wc -l) -ne 0 ]
-	do
-	    iter=$((iter+1))
-	    bams=$(ls | grep -v tmp | head -n 1000)
-	    samtools merge tmp_${iter}.bam ${bams}
-	    rm ${bams}
-	done
+		iter=0
+		cd tmp_files
+		while [ $(ls | grep -v tmp | wc -l) -ne 0 ]
+		do
+			iter=$((iter+1))
+			bams=$(ls | grep -v tmp | head -n 1000)
+			samtools merge tmp_${iter}.bam ${bams}
+			rm ${bams}
+		done
 
-	if [ ${iter} -gt 1 ]
-	then
-		samtools merge - tmp_*.bam | samtools sort -@ threads -m 6000000000 -o ../${case}${type}_merged.bam -
-		rm tmp_*.bam
-	else
-		samtools sort -@ ${threads} -m 6000000000 -o ../${case}${type}_merged.bam tmp_1.bam
-		rm tmp_1.bam
-	fi
-	cd ../
-	samtools index ${case}${type}_merged.bam
+		if [ ${iter} -gt 1 ]
+		then
+			samtools merge - tmp_*.bam | samtools sort -@ threads -m 6000000000 -o ../${case}${type}_merged.bam -
+			rm tmp_*.bam
+		else
+			samtools sort -@ ${threads} -m 6000000000 -o ../${case}${type}_merged.bam tmp_1.bam
+			rm tmp_1.bam
+		fi
+		cd ../
+		samtools index ${case}${type}_merged.bam
 
 
-	time=$(date +%x%t%X)
-	echo ${time}: Bam generation successful "\n"| tee -a pipeline.log
-}
+		time=$(date +%x%t%X)
+		echo ${time}: Bam generation successful "\n"| tee -a pipeline.log
+	}
 
 evaluate(){
 	file=$1
